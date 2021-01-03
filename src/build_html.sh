df2840e2786e0930042a3d602f6caf7f7e6796fa
Ray Ferguson <github.public@devendortech.com>
2021-01-03 17:34:48 -0600

    nojekylllll dogz

--- src/build_html.sh
+++ src/build_html.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f README.rst ]; then
-	sphinx-build -C   \
+	sphinx-build -C   -D extensions=sphinx.ext.githubpages \
 		-D html_show_sphinx=0  -D master_doc=README -D html_theme=haiku -D "html_title=Asus c302ca + Ubuntu 20.04" \
 		-D html_use_index=0 -D html_style=haikuish.css -D html_static_path=src/_static -D html_show_copyright=0 ./ ./docs/
 	cd docs && ln -sf README.html index.html
